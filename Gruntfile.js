6bd80ee24e9d2fcb0d37e91238ea2c703f087ae7
Andrew Rodgers <acedrew@gmail.com>
2014-04-21 03:37:31 +0000

    updated for grunt build process

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+module.exports = function(grunt) {
+ 
+  grunt.registerTask('watch', [ 'watch' ]);
+ 
+  grunt.initConfig({
+    concat: {
+      js: {
+        options: {
+          separator: ';'
+        },
+        src: [
+          'js/core.js',
+          'js/mustache.js'
+        ],
+        dest: 'js/core.min.js'
+      },
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      js: {
+        files: {
+          'js/core.min.js': ['js/core.min.js']
+        }
+      }
+    },
+    sass: {
+      dist: {
+        options: {
+            loadPath: require('node-bourbon').includePaths
+            },
+        files: {
+          "css/screen.css": "sass/screen.scss"
+        }
+      }
+    },
+    watch: {
+      js: {
+        files: ['js/core.js'],
+        tasks: ['concat:js', 'uglify:js'],
+        options: {
+          livereload: true,
+        }
+      },
+      css: {
+        files: ['sass/*.scss'],
+        tasks: ['sass:dist'],
+        options: {
+          livereload: true,
+        }
+      }
+    }
+  });
+ 
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+ 
+};
+
